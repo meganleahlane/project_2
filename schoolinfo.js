b63a97288735c4103fbc46e94538449484870393
Robert Giese <rwg1277@gmail.com>
2020-07-08 18:40:43 -0500

    Wednesday commit

--- schoolinfo.js
+++ schoolinfo.js
@@ -14,35 +14,82 @@ function dropdown_data(){
         var first_sample = names[0];
         school_data(first_sample);
         create_chart(first_sample);
+        create_table(first_sample);
+        dataTable(first_sample);
+        d3.selectAll("#selDataset").on("change", optionChanged);
     });
 }
 dropdown_data()
 
-// function school_data(sampleID){
-//     d3.json ("All_Years_CPS.csv").then((data)=>{
-//     var ??? = ???.???;
-//     var info_array = ???.filter(sample_object => sample_object.id == sampleID);
-//     var result = info_array[0];
-//     var display = d3.select("#moreSchoolInfo");
-//     display.html("");
-//     Object.entries(result).forEach(([key, value])=>{
-//         display.append("h6").text(`${key};${value}`);
-//     });
-//     });
-// }
-
-function optionChanged(sample){
-    // school_data(sample);
-    create_chart(sample);
+function dataTable(data){
+    var tBody = d3.select("tbody");
+    tBody.html("");
+    data.forEach((datarow)=>{
+        var row = tBody.append("tr");
+        Object.values(datarow).forEach((value)=>{
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
 }
 
-function create_chart(sampleID){
-    d3.csv ("All_Years_CPS.csv").then((data)=>{
-        var info_array = data.filter(sample_object => sample_object.short_name == sampleID);
+let rawData;
+
+// var something = info_array = data.filter(sample_object => sample_object.short_name == sampleID);
+
+function create_table(data) {
+    const columnMap = {
+        "year": "Year",
+        "student_growth_rating": "Growth",
+        "student_attainment_rating": "Attainment",
+        "culture_climate_rating": "Culture",
+        "school_survey_safety": "Safety"
+    };
+    const schoolTable = d3.select("#school-table");
+    var first = data[0];
+    var columns = Object.keys(first);
+    const headerRow = schoolTable.select("thead tr");
+    headerRow.html('');
+    columns.forEach(
+        function(column) {
+            if(columnMap[column]) {
+                headerRow.append("td").text(columnMap[column]);
+            }
+        }
+    );
+    var tableBody = schoolTable.select("tbody");
+    tableBody.html('');
+    data.forEach(
+        function(school) {
+            var row = tableBody.append("tr");
+            for (const [key, value] of Object.entries(school)) {
+                if(columnMap[key]) {
+                    row.append("td").text(value);
+                }
+            }
+        }
+    );
+}
+
+d3.csv("All_Years_CPS.csv").then(
+    function(data) {
+        rawData = data;
+    }
+);
+
+function optionChanged(sample){
+    // create_table(rawData);
+    create_chart(sample);
+    var selectedSchoolData = rawData.filter((school) => school.short_name === sample);
+    create_table(selectedSchoolData);
+}
+
+function create_chart(sampleID) {
+        var info_array = rawData.filter(sample_object => sample_object.short_name == sampleID);
         var years = info_array.map(row=>row.year);
         var math = info_array.map(row=>row.attainment_math_pct_es);
         var reading = info_array.map(row=>row.attainment_reading_pct_es);
-        var suspension_rate = info_array.map(row => row.suspensions_per_100_students_avg_pct);
+        var attendance = info_array.map(row => row.student_attendance_avg_pct);
         var trace1 = {
             x: years,
             y: math,
@@ -64,17 +111,16 @@ function create_chart(sampleID){
     }
         var data2 = [{
             x: years, 
-            y: suspension_rate, 
+            y: attendance, 
             type: "scatter",
-            name: "Suspensions"
+            name: "Attendance"
         }];
-        var layout2 = {yaxis: {range: [0, Math.max(suspension_rate)],
-                               title: 'Suspensions'
+        var layout2 = {yaxis: {range: [80, 100],
+                               title: 'Attendance'
         },
-                       title: 'Suspensions per 100 students',
+                       title: 'Attendance rate',
                        xaxis: {title: 'Years'}
     }
         Plotly.newPlot("plot1", data, layout1);
         Plotly.newPlot("plot2", data2, layout2);
-});
 }
